f7fc2567458fd4ff0da7d10ca7ac583d84f6d8de
Alexander Moreno <alexander.f.moreno@gmail.com>
2023-06-10 14:13:42 -0700

    logistic regression using tf-idf

--- interview.py
+++ interview.py
@@ -9,9 +9,14 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-#make a class that initializes with a corpus and has methods to preprocess the corpus and get the top n words for k topics
+#make a class that initializes with a corpus
+#has methods to preprocess the corpus
+#get the top n words for k topics
 class Corpus:
     def __init__(self, corpus, fields, n=3, k=3):
         '''
@@ -22,8 +27,22 @@ class Corpus:
         '''
         self.n = n
         self.k = k
+        self.y = [d['accepted'] for d in corpus]
+        self.y = np.array(self.y)
         corpus = [{k: d[k] for k in fields} for d in corpus]
         self.corpus = [' '.join([d[k] for k in fields]) for d in corpus]
+        self.corpus = self.preprocess_corpus()
+        #get a matrix of word counts
+        self.vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=1000, stop_words='english')
+        self.X = self.vectorizer.fit_transform(self.corpus)
+        #create tf-idf matrix
+        self.tfidf = TfidfTransformer()
+        self.X_tfidf = self.tfidf.fit_transform(self.X).toarray()
+        #get the vocabulary
+        self.vocabulary = self.vectorizer.get_feature_names_out()
+        #split train/test
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.X_tfidf, self.y, test_size=0.2)
+        
     
     def preprocess_corpus(self):
         #remove punctuation
@@ -43,21 +62,29 @@ class Corpus:
         return corpus
     
     def get_top_n_words_for_k_topics(self):
-        #get a matrix of word counts
-        vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=1000, stop_words='english')
-        X = vectorizer.fit_transform(self.corpus)
-        #get the vocabulary
-        vocabulary = vectorizer.get_feature_names_out()
         #use latent dirichlet allocation to find topics
-        lda = LatentDirichletAllocation(n_components=self.k, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(X)
+        self.lda = LatentDirichletAllocation(n_components=self.k, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(self.X)
         #get the top n words for each topic
         important_words = {}
-        for topic_idx, topic in enumerate(lda.components_):
+        for topic_idx, topic in enumerate(self.lda.components_):
             #get the indices of the top n words for each topic
             word_idx = np.argsort(topic)[::-1][:self.n]
             #get the words at those indices
-            important_words[topic_idx] = [vocabulary[i] for i in word_idx]
+            important_words[topic_idx] = [self.vocabulary[i] for i in word_idx]
         return important_words
+    
+    def logistic_regression(self):
+        #fit logistic regression model
+        self.logistic = LogisticRegression(random_state=0).fit(self.x_train, self.y_train)
+        #get accuracy
+        return self.logistic.score(self.x_test, self.y_test)
+
+    def predict(self,idx):
+        #get the idx'th entry in the test set
+        entry = self.x_test[idx]
+        #predict whether the idx'th entry in the test set is accepted or not
+        return self.logistic.predict(entry.reshape(1,-1))
+
 
 #read final_mle_dataset.json from file
 with open('final_mle_dataset.json') as json_file:
@@ -71,14 +98,21 @@ rejected = [d for d in data if d['accepted'] == False]
 
 #create a corpus object from the rejected entries, inputs
 corpus = Corpus(rejected, inputs,5,3)
-rejected_inputs = corpus.preprocess_corpus()
 important_words_inputs = corpus.get_top_n_words_for_k_topics()
+print('topics for failures: inputs')
+print(important_words_inputs)
 
 #create a corpus object from the rejected entries, email
 corpus = Corpus(rejected, ['email'],5,3)
-rejected_email = corpus.preprocess_corpus()
 important_words_email = corpus.get_top_n_words_for_k_topics()
-
-#print the important words for each topic, both inputs and email
-print(important_words_inputs)
+print('topics for failures: outputs')
 print(important_words_email)
+
+#create a corpus object from the rejected entries, critique
+corpus = Corpus(rejected, ['critique'],1,3)
+important_words_critique = corpus.get_top_n_words_for_k_topics()
+
+#create a corpus from all entries, email
+corpus = Corpus(data, ['email'],5,3)
+corpus.logistic_regression()
+print(corpus.predict(0))
